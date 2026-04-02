04b2f7a7dc6655cd3b4ed09431b2b378e89f9b21
BlesUstetu <lionustetu@gmail.com>
2026-04-02 17:14:42 +0700

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import useBinanceWS from "./hooks/useBinanceWS";
-import useAutoSignal from "./hooks/useAutoSignal";
-import Terminal from "./components/Terminal";
-
-export default function App() {
-  useBinanceWS();
-  useAutoSignal();
-  return <Terminal />;
-}
