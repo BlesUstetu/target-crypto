7db08d02286eb4c288f97937076d506a7d3de189
BlesUstetu <lionustetu@gmail.com>
2026-04-04 10:27:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import useWS from "./hooks/useWS";
 import useSMC from "./hooks/useSMC";
 import useAI from "./hooks/useAI";
 import UI from "./components/UI";
+import useDelta from "./hooks/useDelta";
 
 export default function App(){
  useWS();
  useSMC();
  useAI();
+ useDelta();
  return <UI/>;
 }
